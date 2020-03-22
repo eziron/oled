bcb61dc9918e68b61f751825a21f432674b05d16
eziron <kraedon.rf@gmail.com>
2020-03-22 00:25:57 -0300

    Add files via upload

--- GIFlove.py
+++ GIFlove.py
@@ -6,8 +6,10 @@ import numpy as np
 
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=None, i2c_bus=1, gpio=1)
 
-disp.fill(0)
-disp.show()
+disp.begin()
+
+disp.clear()
+disp.display()
 
 imIN = Image.open("GIFlove.gif")
 
@@ -19,16 +21,18 @@ fx = int(imIN.size[1] * f)
 y = 36
 x = 21
 
-for i in range(imIN.n_frames):
-    imIN.seek(i)
-    ip = np.asarray(imIN.resize((fy,fx)))[y:y+32,x:x+128]
-    ipinv = np.zeros(ip.shape)
-    for iy in range(ip.shape[0]):
-        for ix in range(ip.shape[1]):
-            if ip[iy,ix] > 30:
-                ipinv[iy,ix] = 0
-            else:
-                ipinv[iy,ix] = 1
-    
-    disp.image(Image.fromarray(ipinv))
-    disp.show()
+while 1:
+    for i in range(imIN.n_frames):
+        imIN.seek(i)
+        ip = np.asarray(imIN.resize((fy,fx)))[y:y+32,x:x+128]
+        ipinv = np.zeros(ip.shape)
+        for iy in range(ip.shape[0]):
+            for ix in range(ip.shape[1]):
+                if ip[iy,ix] > 30:
+                    ipinv[iy,ix] = 0
+                else:
+                    ipinv[iy,ix] = 1
+
+        disp.image(Image.fromarray(ipinv))
+        disp.display()
+        time.sleep(1)
